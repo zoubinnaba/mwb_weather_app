803b7ecd03bfbc51fc22fc575c3775dd90b70043
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2023-02-11 15:10:30 +0000

    fix fetch api error

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-const API_KEY = '6ec61639cce5288fca2ab1829811fcbe'
+
+const API_KEY = 'ad5b73aff45f94281b2076c5c15d7f5d'
 
 const inputEl = document.getElementById("query")
 const submitEl = document.getElementById("search")
+const weatherInfoEl = document.querySelector('.ajax-section');
+const msgEl = document.querySelector('.msg');
 
 let CITY = '';
 
@@ -12,51 +15,29 @@ inputEl.addEventListener("change", function() {
 submitEl.addEventListener("click", function(event) {
     event.preventDefault();
     const UNITS = "metric";
+    if (!CITY) {
+        msgEl.innerHTML = 'Please enter a city name';
+        return;
+    }
 
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNITS}`;
 
     fetch(API_URL)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-});
-
-
-
-/*
-const API_KEY = 'YOUR_API_KEY_HERE';
-const UNITS = 'metric';
-
-const inputEl = document.getElementById('query');
-const submitEl = document.getElementById('search');
-const weatherInfoEl = document.querySelector('.weather-info');
-const msgEl = document.querySelector('.msg');
-
-submitEl.addEventListener('click', function(event) {
-    event.preventDefault();
-    const city = inputEl.value;
-    if (!city) {
-        msgEl.innerHTML = 'Please enter a city name';
-        return;
-    }
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${UNITS}`;
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            msgEl.innerHTML = '';
+        console.log(data.main);
+        msgEl.innerHTML = '';
             weatherInfoEl.innerHTML = `
                 <li>Temperature: ${data.main.temp} &#8451;</li>
                 <li>Humidity: ${data.main.humidity}%</li>
                 <li>Wind Speed: ${data.wind.speed} m/s</li>
                 <li>Description: ${data.weather[0].description}</li>
-            `;
-        })
-        .catch(error => {
-            msgEl.innerHTML = 'City not found. Please try again.';
-        });
+            `
+      })
+      .catch(error => {
+        msgEl.innerHTML = 'City not found. Please try again.';
+      });
 });
-*/
+
+
+
