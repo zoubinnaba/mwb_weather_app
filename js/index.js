c1529f60f3d4dd32ee502c8cfe4b45e9045c7e57
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2023-02-12 07:12:03 +0000

    add geolocation feature

--- js/index.js
+++ js/index.js
@@ -29,32 +29,54 @@ submitEl.addEventListener("click", function(event) {
     // Show the weather information section
     weatherInfoEl.style.display = "block"
     // Define the API URL with the city name, API key, and units
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNITS}`;
+    //const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNITS}`;
     // Make a fetch API call to get the weather data
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-        // Log the data in the console
-        console.log(data.main);
-        // Clear the error message
-        msgEl.innerHTML = '';
-        // Update the weather information section with the data
+    let API_URL
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${UNITS}`
+        fetch(API_URL)
+          .then(respose => respose.json())
+          .then(data => {
+            msgEl.textContent = ''
+            console.log(data)
             weatherInfoEl.innerHTML = `
-                <li>Temperature: ${data.main.temp} <span class="material-symbols-outlined">thermometer</span></li>
-                <li>Humidity: ${data.main.humidity}<span class="material-symbols-outlined">
-                humidity_percentage
-                </span></li>
-                <li>Wind Speed: ${data.wind.speed}<span class="material-symbols-outlined">
-                avg_pace
-                </span></li>
-                <li>Description: ${data.weather[0].description}</li>
+              <li>Temperature: ${data.main.temp} <span class="material-symbols-outlined">thermometer</span></li>
+              <li>Humidity: ${data.main.humidity}<span class="material-symbols-outlined">
+              humidity_percentage
+              </span></li>
+              <li>Wind Speed: ${data.wind.speed}<span class="material-symbols-outlined">
+              avg_pace
+              </span></li>
+              <li>Description: ${data.weather[0].description}</li>
             `
+          })
+          .catch(error => {
+            msgEl.textContent = 'City not found. Please try again.'
+          })
       })
-      .catch(error => {
-        // Show an error message if the city is not found
-        msgEl.innerHTML = 'City not found. Please try again.';
-      });
+    } else {
+      API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNITS}`
+
+      fetch(API_URL)
+        .then(response => response.json())
+        .then(data => {
+          msgEl.textContent = ''
+          weatherInfoEl. innerHTML = `
+            <li>Temperature: ${data.main.temp} <span class="material-symbols-outlined">thermometer</span></li>
+            <li>Humidity: ${data.main.humidity}<span class="material-symbols-outlined">
+            humidity_percentage
+            </span></li>
+            <li>Wind Speed: ${data.wind.speed}<span class="material-symbols-outlined">
+            avg_pace
+            </span></li>
+            <li>Description: ${data.weather[0].description}</li>
+          `
+        })
+        .catch(error => {
+          msgEl.textContent = 'City not found. Please try again.'
+        })
+    }
 });
-
-
-
